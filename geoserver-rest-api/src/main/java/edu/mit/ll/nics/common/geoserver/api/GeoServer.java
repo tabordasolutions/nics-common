e8d003c38d964421b6269e518e2babda544ce3a7
snagole <sneha.nagole@tabordasolutions.com>
2018-09-28 15:00:58 -0700

    Refactor to throw exception in case of errors

--- geoserver-rest-api/src/main/java/edu/mit/ll/nics/common/geoserver/api/GeoServer.java
+++ geoserver-rest-api/src/main/java/edu/mit/ll/nics/common/geoserver/api/GeoServer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008-2016, Massachusetts Institute of Technology (MIT)
+ * Copyright (c) 2008-2018, Massachusetts Institute of Technology (MIT)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -35,8 +35,8 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.sun.deploy.util.StringUtils;
 import com.vividsolutions.jts.geom.Coordinate;
+import org.apache.commons.codec.binary.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -540,10 +540,28 @@ public class GeoServer extends SuperGeoServer {
         return restPost("/gwc/rest/seed/" + featureTypeName, resetXML, "text/xml");
     }
 
-    public String getFeatureDetails(String featureTypeName, Coordinate coordinate, String CRS,  List<String> returnProperties, String geometryColumn) {
+    public String getFeatureDetails(String featureTypeName, Coordinate coordinate, String CRS,  List<String> returnProperties, String geometryColumn) throws Exception {
         String requestUrl = "/wfs?service=WFS&request=GetFeature&version=1.0.0&typeName=" + featureTypeName +
-                "&outputFormat=JSON&CQL_FILTER=contains%28" + geometryColumn + ",%20POINT%20%28" + coordinate.x + "%20" + coordinate.y + "%29%29&&srsname=%s&" +
-                "propertyName=" + StringUtils.join(returnProperties, ",");
-        return restGet(requestUrl, "application/json");
+                "&outputFormat=JSON&CQL_FILTER=contains%28" + geometryColumn + ",%20POINT%20%28" + coordinate.x + "%20" + coordinate.y + "%29%29&&srsname=%s&";
+        String returnPropertiesString = joinStringsByDelimiter(returnProperties);
+        if(returnPropertiesString != null)
+                requestUrl = requestUrl + "propertyName=" + this.joinStringsByDelimiter(returnProperties);
+        String response = restGet(requestUrl, "application/json");
+        if(response.contains("<ServiceExceptionReport")) {
+            log.log(Level.SEVERE, "Unable to fetch feature details from feature type " + featureTypeName + ", Error details: " + response);
+            throw new Exception("Unable to fetch feature details from feature type " + featureTypeName + ", ErrorMessage: " + response);
+        }
+        return response;
+    }
+
+    public String joinStringsByDelimiter(List<String> list) {
+        if(list == null || list.isEmpty()) {
+            return null;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (String s : list) {
+            builder.append(s).append(",");
+        }
+        return builder.substring(0, builder.length()-1);
     }
 }
