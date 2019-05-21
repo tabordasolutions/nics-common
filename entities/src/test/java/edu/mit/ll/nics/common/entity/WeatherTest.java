1d3077fdcbcf768990311f1df3d01d0dd00bd166
Nikhil Devre <nikhil.devre@tabordasolutions.com>
2019-05-21 10:39:03 -0700

    Wind direction changed to abbreviated Cardinal values
    - Added new function getDescriptiveWindDirectionAbbreviation() to Weather class for abbreviated cardinal values.

--- entities/src/test/java/edu/mit/ll/nics/common/entity/WeatherTest.java
+++ entities/src/test/java/edu/mit/ll/nics/common/entity/WeatherTest.java
@@ -98,4 +98,59 @@ public class WeatherTest {
         weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 360.0+170, 3.01f, "OK", 8.012);
         assertEquals(weather.getDescriptiveWindDirection(), "South");
     }
+
+    //getDescriptiveWindDirectionAbbreviation
+
+    @Test
+    public void verifyAbbreviatedDirectionDegreesForNorth() {
+        Weather weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 0.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "N");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 10.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "N");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 360.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "N");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 337.5, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "N");
+    }
+
+    @Test
+    public void verifyAbbreviatedDirectionDegreesForNorthEast() {
+        Weather weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 22.5, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "NE");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 32.5, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "NE");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 45.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "NE");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 65.5, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "NE");
+    }
+
+    @Test
+    public void verifyAbbreviatedDirectionDegreesForEast() {
+        Weather weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 68.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "E");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 75.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "E");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 90.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "E");
+    }
+
+    @Test
+    public void verifyAbbreviatedCompassDirectionForNorthWest() {
+        Weather weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 315.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "NW");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 335.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "NW");
+    }
+
+    @Test
+    public void testDegreesAreNormalizedBeforeCalculatingAbbreviatedCompassDirection() {
+        Weather weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 360.0+275.0, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "W");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 360.0+220, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "SW");
+        weather = new Weather("objectid", "POINT(-121.0909 35.009893)", 80.01, 2.0f, 360.0+170, 3.01f, "OK", 8.012);
+        assertEquals(weather.getDescriptiveWindDirectionAbbreviation(), "S");
+    }
+
 }
